ac92e5ebe4362bea4027b66d59ba0c480b553440
Sxhilllll <sahilyadav1005@gmail.com>
2025-07-21 20:04:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+var timer= 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore(){
+    score += 10; 
+    document.querySelector("#scoreval").textContent= score;
+}
+function decScore(){
+    if(score>0){
+    score -=10; 
+    document.querySelector("#scoreval").textContent= score;
+}}
+
+
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent= hitrn;
+}
+
+function makeBubble(){
+    var clutter = "";
+
+for(var i =1;i<=126; i++){
+    var rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+document.querySelector(".pbtm").innerHTML = clutter;
+}
+
+function runTimer(){
+    var timerint = setInterval(function(){
+        if (timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent= timer;
+        } else{
+            clearInterval(timerint);
+            document.querySelector(".pbtm").innerHTML= `<h1>Game Over</h1>`;
+        }
+    },1000);
+}
+
+document.querySelector(".pbtm").addEventListener("click", function(dets){
+    var clickedNum = Number(dets.target.textContent);
+    if(clickedNum === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    } else{
+        decScore();
+    }
+})
+
+runTimer();
+makeBubble();
+getNewHit();
+
+
